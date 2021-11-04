ffdfdc0fdb22fb09ae61fe72a3499b3ba0883e3a
Leonardo Felix Correa <leonardo.correa@compasso.com.br>
2021-11-03 23:05:54 -0300

    Add new style and Fix current style to main page
    Related to StyleV1.0

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -1,14 +1,26 @@
-function processa() {
+function traduz() {
   var conteudo = document.getElementById("taConteudo").value;
-  var traducao = document.getElementById("taTraducao");
   var vetor = conteudo.split("\n");
   var i = 0;
   for (i; i < vetor.length; i++) {
     alert("linha " + (i + 1) + " => " + vetor[i]);
     if (vetor[i] == "int main() {") {
-      traducao.value = "PUSH %23";
-      // IMPORTANTE document.getElementById("teste").innerHTML = 
-      // IMPORTANTE document.getElementById("teste").innerHTML + "<span>PUSH %12</span> <br>";
+      // traducao.value = "PUSH %23";
+      document.getElementById("traducao").innerHTML =
+        document.getElementById("traducao").innerHTML +
+        "<span>PUSH %12</span> <br>";
+    } else if (vetor[i] == "int a;") {
+      document.getElementById("traducao").innerHTML =
+        document.getElementById("traducao").innerHTML +
+        "<span>INT A AQUI</span> <br>";
+    } else if (vetor[i] == "cin>>a;") {
+      document.getElementById("traducao").innerHTML =
+        document.getElementById("traducao").innerHTML +
+        "<span>CIN A AQUI</span> <br>";
+    } else if (vetor[i] == "}") {
+      document.getElementById("traducao").innerHTML =
+        document.getElementById("traducao").innerHTML +
+        "<span>FINAL</span> <br>";
     }
   }
   alert.dismiss();
@@ -16,6 +28,9 @@ function processa() {
 }
 
 function temaClaro() {
-  var element = document.body;
-  element.classList.toggle("temaClaro");
+  document.body.classList.toggle("body-dark");
+  document.getElementById("btn").classList.toggle("btn-main");
+  document.getElementById("btn").classList.toggle("btn-main-dark");
+  document.getElementById("btn-2").classList.toggle("btn-main");
+  document.getElementById("btn-2").classList.toggle("btn-main-dark");
 }
