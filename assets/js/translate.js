74077cbf4b5e7055c9df638789549ba7ddc2fedb
Leonardo Felix Correa <leonardo.correa@compasso.com.br>
2021-11-12 22:10:27 -0300

    Update Functionality on translator
    Relates to Pi4-2

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -7,26 +7,24 @@ function traduz() {
     if (vetor[i] == "int main() {") {
       document.getElementById("traducao").innerHTML =
         document.getElementById("traducao").innerHTML +
-        "<span>PUSH %BP</span> <br>" +
-        "<span>MOV  %SP, %BP</span> <br>";
+        "<span class=\"intMain\">int main: OK</span> <br>";
     } else if (vetor[i] == "int a;") {
       document.getElementById("traducao").innerHTML =
         document.getElementById("traducao").innerHTML +
-        "<span>INT A AQUI</span> <br>";
-    } else if (vetor[i] == "cin>>a;") {
+        "<span>int a: OK</span> <br>";
+    } else if (vetor[i] == "cin >> a" || vetor[i] == "cin >> a;") {
       document.getElementById("traducao").innerHTML =
         document.getElementById("traducao").innerHTML +
-        "<span>CIN A AQUI</span> <br>";
+        "<span>cin >> a: OK</span> <br>";
     } else if (vetor[i] == "}") {
       document.getElementById("traducao").innerHTML =
         document.getElementById("traducao").innerHTML +
-        "<span>FINAL</span> <br>";
+        "<span>}: OK</span> <br>";
     }
   }
   alert.dismiss();
   return false;
 }
-
 function temaClaro() {
   document.body.classList.toggle("body-dark");
   document.getElementById("btn").classList.toggle("btn-main");
@@ -34,3 +32,14 @@ function temaClaro() {
   document.getElementById("btn-2").classList.toggle("btn-main");
   document.getElementById("btn-2").classList.toggle("btn-main-dark");
 }
+
+// document.querySelector(function () {
+//   document.getElementsByClassName("intMain").hover(
+//     function () {
+//     },
+//     function () {
+//       // on mouseout, reset the background colour
+//       document.querySelector("#b").css("background-color", "");
+//     }
+//   );
+// });
